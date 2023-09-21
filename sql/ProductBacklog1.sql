dfa255620cc626840b1534a2d435e73fd12eea86
Odhran <odhran.hague@kainos.com>
2023-09-21 10:19:00 +0100

    PB1 creating sales employee table

--- sql/ProductBacklog1.sql
+++ sql/ProductBacklog1.sql
@@ -1,4 +1,14 @@
 USE db_PICASO_OdhranH;
 
+CREATE TABLE SalesEmployee (
+  SaleID int AUTO_INCREMENT PRIMARY KEY,
+  Forename varchar(20) NOT NULL,
+  Surname Varchar(20) NOT NULL,
+  Salary decimal(9,2) NOT NULL,
+  BAN varchar(16) NOT NULL,
+  NINumber varchar(9) NOT NULL,
+  ComRate decimal(9,2) NOT NULL
+);
+
 INSERT INTO SalesEmployee(Forename, Surname, Salary, BAN, NINumber, ComRate)
 VALUES('Odhran', 'Hague', 60000.00, 123456, 'PK093205B', 4.5);
