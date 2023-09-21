4c880ba8c086b1dd6ec6b910b0d6ae3a743a616b
Odhran <odhran.hague@kainos.com>
2023-09-21 15:45:21 +0100

    Refactoring

--- sql/TotalCommision.sql
+++ sql/TotalCommision.sql
@@ -1,24 +1,6 @@
-ALTER TABLE SalesEmployee RENAME COLUMN SaleID TO SalesEmpID;
-
-SELECT * FROM Project;
-SELECT * FROM SalesEmployee;
-
-ALTER TABLE Project
-ADD column SalesEmpID INT;
-
-ALTER TABLE Project
-ADD FOREIGN KEY(SalesEmpID) REFERENCES SalesEmployee(SalesEmpID);
-
 -- As a member of the HR team I want to see a list of sales employees and 
 -- how much each has made in commission this year
-
-SELECT CONCAT(Forename,' ', Surname) as 'Name', ProjectValue, ComRate FROM SalesEmployee s
-INNER JOIN Project p on p.SalesEmpID = s.SalesEmpID;
 
 SELECT CONCAT(Forename,' ', Surname) as 'Name', SUM((ProjectValue/100) * ComRate) as 'Commision Paid' FROM SalesEmployee s
 INNER JOIN Project p on p.SalesEmpID = s.SalesEmpID
 GROUP BY CONCAT(Forename,' ', Surname);
-
-
-
-
