431ee75de64a2242fe5e52cd60168b2a12f7f050
Caitlin Bulloch <caitlinbulloch@c02ff0f2ml85.client.kainos.com>
2023-09-21 10:23:29 +0100

    Add file

--- sql/Product-Backlog-3.sql
+++ sql/Product-Backlog-3.sql
@@ -1,4 +1,12 @@
-use db_PICASO_OdhranH;
+USE db_PICASO_OdhranH;
+
+CREATE TABLE Client (
+  ClientID int AUTO_INCREMENT PRIMARY KEY,
+    Forename varchar(20) NOT NULL,
+    Surname varchar(20) NOT NULL,
+    Address varchar(50) NOT NULL,
+    Phone varchar(15) NOT NULL
+);
 
 INSERT INTO Client (Forename, Surname, Address, Phone)
 VALUES ('Nicole','Brown','12 Drumstick lane','174882123');
