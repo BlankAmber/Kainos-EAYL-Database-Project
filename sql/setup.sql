09d2a03a13e1555ba62d5ae446cd9e928f00602b
Aideen McHugh <aideenmchugh@c02gf0bwml85.client.kainos.com>
2023-09-21 10:50:29 +0100

    Adds sql data for creating link table and adding employee project links

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,14 @@
 CREATE DATABASE db_PICASO_OdhranH;
 USE db_PICASO_OdhranH;
+
+
+CREATE TABLE Project_DeliveryEmployee(
+	EmployeeID int,
+	ProjectID int,
+	isTechLead boolean NOT NULL,
+	startDate dateTime NOT NULL DEFAULT NOW(),
+	endDate dateTime NULL,	
+	PRIMARY KEY(EmployeeID, ProjectID),
+	Foreign Key(EmployeeID) references DeliveryEmployee(DEmpID),
+	FOREIGN KEY (ProjectID) references Project(ProjectID)
+);
