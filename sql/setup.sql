9d55c4839a81f0f1fe591c3f30077084f29c073d
Aideen McHugh <aideenmchugh@c02gf0bwml85.client.kainos.com>
2023-09-21 10:19:51 +0100

    Added create statement to setup.sql file

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,11 @@
 CREATE DATABASE db_PICASO_OdhranH;
 USE db_PICASO_OdhranH;
+
+# SQL for creating Delivery Employee table
+CREATE TABLE DeliveryEmployee(
+DEmpID int auto_increment not null PRIMARY KEY,
+DEmpForename varchar(20) not null,
+DEmpSurname varchar(20) not null,
+DEMPBankNum VARCHAR(16) not null,
+NationalInsuranceNum Varchar(9) not null
+);
