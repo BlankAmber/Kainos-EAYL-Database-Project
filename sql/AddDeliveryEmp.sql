187690d6cd3e005778a05447f0d9edfb96bce20f
Aideen McHugh <aideenmchugh@c02gf0bwml85.client.kainos.com>
2023-09-21 10:06:18 +0100

    Created Add Delivery Employee sql file

---
+++ sql/AddDeliveryEmp.sql
@@ -0,0 +1,11 @@
+# Use this SQL file to add new Delivery Employees to the database.
+# Change values in brackets
+#
+# In order to add multiple, copy line and change values
+# EG:
+# Insert into DeliveryEmployee(DEmpForename, DEmpSurname, DEmpBankNum, NationalInsuranceNum) VALUES
+# ('Forename','Surname','PhoneNum', 'NI Num'),
+# ('Forename','Surname','PhoneNum', 'NI Num');
+#
+Insert into DeliveryEmployee(DEmpForename, DEmpSurname, DEmpBankNum, NationalInsuranceNum) VALUES
+('Forename','Surname','PhoneNum', 'NI Num');
