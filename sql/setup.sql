9626032a988248c391dcbdebf0f6513cff9de80a
Aideen McHugh <aideenmchugh@c02gf0bwml85.client.kainos.com>
2023-09-21 14:01:30 +0100

    Changed setup.sql: CREATE Project and Project_DeliveryEmployee commands

--- sql/setup.sql
+++ sql/setup.sql
@@ -11,33 +11,49 @@ CREATE TABLE DeliveryEmployee(
     );
 
 CREATE TABLE Project (
-  ProjectID int NOT NULL AUTO_INCREMENT,
-  ProjectName varchar(50) NOT NULL,
-  ProjectValue decimal(9,2) NOT NULL,
-  ClientID int,
+  ProjectID INT NOT NULL AUTO_INCREMENT,
+  ProjectName VARCHAR(50) NOT NULL,
+  ProjectValue DECIMAL(9,2) NOT NULL,
+  ClientID INT,
+  TechLeadID INT NOT NULL,
   PRIMARY KEY (ProjectID),
-  CONSTRAINT FOREIGN KEY (ClientID) REFERENCES Client (ClientID)
+  CONSTRAINT FOREIGN KEY (ClientID) REFERENCES `Client`(ClientID),
+  CONSTRAINT FOREIGN KEY  (TechLeadID) REFERENCES DeliveryEmployee(DEmpID)
 );
 
 CREATE TABLE Technologies (
-  TechID int AUTO_INCREMENT NOT NULL PRIMARY KEY,
-  TechType varchar(50) NOT NULL
+  TechID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  TechType VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE Project_Tech (
   ProjectID int NOT NULL,
   TechID int NOT NULL,
   PRIMARY KEY (ProjectID,TechID),
-  CONSTRAINT FOREIGN KEY (ProjectID) REFERENCES Project (ProjectID),
-  CONSTRAINT FOREIGN KEY (TechID) REFERENCES Technologies (TechID)
+  CONSTRAINT FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID),
+  CONSTRAINT FOREIGN KEY (TechID) REFERENCES Technologies(TechID)
 );
 
 CREATE TABLE SalesEmployee (
-  SaleID int AUTO_INCREMENT PRIMARY KEY,
-  Forename varchar(20) NOT NULL,
-  Surname Varchar(20) NOT NULL,
-  Salary decimal(9,2) NOT NULL,
-  BAN varchar(16) NOT NULL,
-  NINumber varchar(9) NOT NULL,
-  ComRate decimal(9,2) NOT NULL
+  SaleID INT AUTO_INCREMENT PRIMARY KEY,
+  Forename VARCHAR(20) NOT NULL,
+  Surname VARCHAR(20) NOT NULL,
+  Salary DECIMAL(9,2) NOT NULL,
+  BAN VARCHAR(16) NOT NULL,
+  NINumber VARCHAR(9) NOT NULL,
+  ComRate DECIMAL(9,2) NOT NULL
 );
+
+
+
+CREATE TABLE Project_DeliveryEmployee(
+	EmployeeID INT,
+	ProjectID INT,
+	startDate DATE NOT NULL DEFAULT NOW(),
+	endDate DATETIME NULL,
+	PRIMARY KEY(EmployeeID, ProjectID),
+	FOREIGN KEY (EmployeeID) REFERENCES DeliveryEmployee(DEmpID),
+	FOREIGN KEY (ProjectID) REFERENCES Project(ProjectID),
+	
+);
+
