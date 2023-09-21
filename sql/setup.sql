52ea58c16e3539bbb851965d446e59fc884b5d17
Caitlin Bulloch <caitlinbulloch@c02ff0f2ml85.client.kainos.com>
2023-09-21 10:26:00 +0100

    Add create table

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,10 @@
 CREATE DATABASE db_PICASO_OdhranH;
 USE db_PICASO_OdhranH;
+
+CREATE TABLE Client (
+  ClientID int AUTO_INCREMENT PRIMARY KEY,
+    Forename varchar(20) NOT NULL,
+    Surname varchar(20) NOT NULL,
+    Address varchar(50) NOT NULL,
+    Phone varchar(15) NOT NULL
+);
